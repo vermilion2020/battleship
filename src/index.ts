61004dfef3d014df8a74402837d5deee5f1b843f
Militsa Tuseeva <militsa.tuseeva.ext@abtasty.com>
2025-05-18 22:54:16 +0200

    feat: display ws parameters on start listenning

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,19 @@ import { BASE_PORT } from './utils/consts';
 import { messageHandler } from './utils/message-handler';
 import 'dotenv/config';
 import { GameSessionRepository } from './repository/game-session';
+import { setColor } from './utils/logger';
 
 const PORT = +process.env.PORT || BASE_PORT;
 
 const wss = new WebSocketServer({ port: PORT });
 const gameSessionRepository = new GameSessionRepository();
+
+wss.on('listening', () => {
+  console.log('\nWebSocket Server Parameters:');
+  console.log(`Port: ${setColor(PORT.toString(), 'green')}`);
+  console.log(`Protocol: ${setColor('ws', 'green')}`);
+  console.log(`URL: ${setColor(`ws://localhost:${PORT}`, 'green')}\n`);
+});
 
 wss.on('connection', (ws) => {
   ws.on('message', (rawMessage) => {
